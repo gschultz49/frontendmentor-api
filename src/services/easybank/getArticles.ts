b52789f1e9b6578764ca5696bd4ae644e5ace7ed
Greg Schultz <gjs86@cornell.edu>
2020-04-04 20:25:41 -0400

    remove unused services (these are in the web tier)

--- src/services/easybank/getArticles.ts
+++
@@ -1,9 +0,0 @@
-import { getEndpoint } from "./easybank-api-utils";
-import { fetcher } from "../utils";
-
-export const getArticlesFileName = "/get-articles";
-
-export default async function GetArticlesGridData() {
-  const fullEndpointUrl = getEndpoint(getArticlesFileName);
-  return await fetcher(fullEndpointUrl);
-}
--- src/services/easybank/getWhyChoose.ts
+++
@@ -1,9 +0,0 @@
-import { getEndpoint } from "./easybank-api-utils";
-import { fetcher } from "../utils";
-
-export const getWhyChooseFileName = "/get-whyChoose";
-
-export default async function GetWhyChooseData() {
-  const fullEndpointUrl = getEndpoint(getWhyChooseFileName);
-  return await fetcher(fullEndpointUrl);
-}
